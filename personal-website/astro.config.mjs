a76efe97e3bef55ca6530b30995b17035a8b5430
Ray Xia <xia.t@wustl.edu>
2024-11-12 11:36:05 -0500

    remove jekyll

--- personal-website/astro.config.mjs
+++ personal-website/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://xiatiantian24.github.io/pui-homework/personal-website',
+  site: 'https://xiatiantian24.github.io/',
   integrations: [tailwind()]
 });
