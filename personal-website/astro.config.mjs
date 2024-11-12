cf5a608f58e56ff5e1250f80916d11450841bacf
Ray Xia <xia.t@wustl.edu>
2024-11-12 11:38:34 -0500

    Update astro.config.mjs

--- personal-website/astro.config.mjs
+++ personal-website/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://xiatiantian24.github.io/',
+  site: 'https://xiatiantian24.github.io/pui-homework/personal-website',
   integrations: [tailwind()]
 });
